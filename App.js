ff5145724813008745c25f4c49a3b70e6f73acaa
prachi-pra <tprachi761@gmail.com>
2025-08-09 23:34:50 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+import Tours from './componenets/Tours';
+import data from "./data";
+
+function App() {
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id){
+    const newTour = tours.filter(tour => tour.id !== id);
+    setTours(newTour);
+  }
+
+  if(tours.length === 0){
+    return (
+      <div className='refresh'>
+        <h2>No Tours Left</h2>
+        <button className='btn-white' onClick={ () => setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
---
+++ data.jsx
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id: 1,
+        name: "Agra",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 2,
+        name: "jaipur",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 3,
+        name: "london",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 4,
+        name:"paris",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 5,
+        name:"kolkata",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 6,
+        name: "mumbai",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+    {
+        id: 7,
+        name: "delhi",
+        info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis aliquam officia ea, nobis iusto praesentium doloribus temporibus asperiores sit distinctio hic doloremque quia eos libero voluptatibus dolorem aut laudantium harum.",
+        image: "https://plus.unsplash.com/premium_photo-1683800241997-a387bacbf06b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhdmVsJTIwdG91cnxlbnwwfHwwfHx8MA%3D%3D",
+        price: "32,789",
+    },
+]
+export default data;
